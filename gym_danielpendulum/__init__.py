4ba3c91cf641af1d1e9dbdac075750def1bec82d
Daniel McClement <daniel.mcclement@gmail.com>
2020-11-24 23:07:09 -0800

    Create __init__.py

---
+++ gym_danielpendulum/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 22:40:03 2020
+
+@author: Daniel McClement
+"""
+
+
+from setuptools import setup
+
+setup(name='danielpendulum',
+      version='0.0.1',
+      install_requires=['gym']  # And any other dependencies foo needs
+)
