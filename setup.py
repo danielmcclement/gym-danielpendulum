2b5adc998cc333a18fbc5c92e7f6463611670dfe
Daniel McClement <daniel.mcclement@gmail.com>
2020-11-24 23:32:52 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Tue Nov 24 22:40:03 2020
 
 from setuptools import setup
 
-setup(name='danielpendulum',
+setup(name='gym_danielpendulum',
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies foo needs
 )
